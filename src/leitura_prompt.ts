f19810101c012702ed8d5c6681e54a0e23e217f0
RafaGR <110624346+RaffaGR@users.noreply.github.com>
2026-02-27 20:26:50 -0300

    2.4

---
+++ src/leitura_prompt.ts
@@ -0,0 +1,13 @@
+import promptSync from "prompt-sync";
+
+// Inicializa a função prompt
+const prompt = promptSync();
+
+const nome: string = prompt(" Digite seu nome: ");
+const idade: string = prompt(" Digite sua idade: ");
+
+console.log(`Olá , ${nome}! Você tem ${idade} anos .`);
+
+// Importante: O prompt sempre retorna uma string
+// Se precisar de numero, use: Number (prompt("..."));
+
